bc1e6086a4fcbb43079bf232b2b5767a255f5de6
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-22 17:29:59 +1100

    Add NavigationLinks for hearing and mobility disability views to UnderstandingDisabilityView

--- SwiftStudentChallenge2024.swiftpm/UnderstandingDisabilityView.swift
+++ SwiftStudentChallenge2024.swiftpm/UnderstandingDisabilityView.swift
@@ -15,9 +15,9 @@ struct UnderstandingDisabilityView: View {
                 .padding()
             Text("Disability is a diverse and multifaceted condition that affects individuals in various ways, impacting their ability to engage in daily activities, communication, and interaction with the world around them. Disabilities can be present from birth or acquired due to illness or injury. This app aims to promote empathy and inclusivity by shedding light on the different experiences of those living with disabilities.")
                 .padding()
-            // Add more info on each of the major types of disability
             NavigationLink("Visual Disabilities", destination: VisualDisabilityView())
-            
+            NavigationLink("Hearing Disabilities", destination: HearingDisabilityView())
+            NavigationLink("Mobility Disabilities", destination: MobilityDisabilityView())
         }
         .navigationTitle("Disabilities")
     }
