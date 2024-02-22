0db1f8ed834ac6a1a894ad7ebdf5cc19906a1ab0
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-22 20:26:24 +1100

    Add VisualDisabilityFilter enum to use in flag variable for switching visual disability simulations

---
+++ SwiftStudentChallenge2024.swiftpm/CameraFeedProcessor.swift
@@ -0,0 +1,8 @@
+//
+//  CameraFeedProcessor.swift
+//  SwiftStudentChallenge2024
+//
+//  Created by Anh Nguyen on 22/2/2024.
+//
+
+import Foundation
---
+++ SwiftStudentChallenge2024.swiftpm/VisualDisabilityFilter.swift
@@ -0,0 +1,21 @@
+//
+//  VisualDisabilityFilter.swift
+//  SwiftStudentChallenge2024
+//
+//  Created by Anh Nguyen on 22/2/2024.
+//
+
+import Foundation
+
+enum VisualDisabilityFilter {
+    case none
+    // Red blind
+    case protanopia
+    // Green blind
+    case deuteranopia
+    // Blue-yellow blind
+    case tritanopia
+    // Completely colour blind
+    case achromatopsia
+    case lowVision
+}
