ded67a61b05118214b1a135e8e1793fce7fbeff7
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-22 15:30:06 +1100

    Add AppOverviewView to introduce the app

---
+++ SwiftStudentChallenge2024.swiftpm/AppOverviewView.swift
@@ -0,0 +1,18 @@
+//
+//  AppOverviewView.swift
+//  Test
+//
+//  Created by Anh Nguyen on 22/2/2024.
+//
+
+import SwiftUI
+
+struct AppOverviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppOverviewView()
+}
