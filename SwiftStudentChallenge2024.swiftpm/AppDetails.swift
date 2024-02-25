0d3c828f8dd52930ae2dab303ccb3879d49d6ed4
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-25 15:47:54 +1100

    Update AppOverView copy and styling

--- SwiftStudentChallenge2024.swiftpm/AppDetails.swift
+++ SwiftStudentChallenge2024.swiftpm/AppDetails.swift
@@ -5,6 +5,10 @@
 //  Created by Anh Nguyen on 25/2/2024.
 //
 
+import SwiftUI
+
 class AppDetails {
     static let name = "Eye Engage"
+    static let accentPrimary = Color(red: 0.627, green: 0.094, blue: 1)
+    static let accentSecondary = Color(red: 0.933, green: 0.929, blue: 1)
 }
--- SwiftStudentChallenge2024.swiftpm/AppOverviewView.swift
+++ SwiftStudentChallenge2024.swiftpm/AppOverviewView.swift
@@ -9,14 +9,20 @@ import SwiftUI
 
 struct AppOverviewView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Welcome to Accessibility Quest!")
-                .font(.title)
-                .padding()
-            Text("Dive into a world where every challenge is an opportunity to explore Apple's accessibility features through engaging gameplay. But that's not all – our game also introduces you to the principles of accessible design, empowering you to create more inclusive digital environments. Whether you're familiar with accessibility features or exploring them for the first time, Accessibility Quest offers a fun, interactive way to learn, play, and grow. Come on a journey to make the digital world accessible to everyone!")
-                .padding()
+        ZStack {
+            AppDetails.accentSecondary
+                .ignoresSafeArea()
+            VStack(alignment: .center) {
+                (Text("Eye ") + Text("Engage").bold())
+                    .font(.largeTitle)
+                Text("Experience simulators and mini games to better understand visual impairment")
+                Button("Lets play!") {
+                    
+                }
+                .buttonBorderShape(.capsule)
+                .buttonStyle(.borderedProminent)
+            }
         }
-        .navigationTitle("App Overview")
     }
 }
 
