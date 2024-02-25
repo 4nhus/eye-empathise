f23d75ae90d6996d82f738ee3044760a0fc077d6
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-02-25 17:03:34 +1100

    Add SimulationRationaleView to explain how the visual disability simulator fosters empathy

--- SwiftStudentChallenge2024.swiftpm/ConsentView.swift
+++ SwiftStudentChallenge2024.swiftpm/ConsentView.swift
@@ -26,6 +26,7 @@ struct ConsentView: View {
                 
                 .buttonStyle(.bordered)
                 NavigationLink("Let's play!") {
+                    SimulationRationaleView()
                 }
                 .buttonStyle(.borderedProminent)
             }
---
+++ SwiftStudentChallenge2024.swiftpm/SimulationRationaleView.swift
@@ -0,0 +1,38 @@
+//
+//  SimulationRationaleView.swift
+//  SwiftStudentChallenge2024
+//
+//  Created by Anh Nguyen on 25/2/2024.
+//
+
+import SwiftUI
+
+struct SimulationRationaleView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Why visual simulation matters")
+                .font(.largeTitle)
+                .bold()
+                .padding(.bottom)
+            Text("Visual disabilities vary widely, impacting individuals in unique ways. Reading about these conditions can provide knowledge, but experiencing them—even virtually—offers profound insights that words alone cannot convey.")
+                .padding(.bottom)
+
+            Text("By seeing the world through the lens of different visual impairments, you're not just learning about the challenges; you're experiencing a fraction of what many navigate daily. This immersive insight fosters empathy, transforming abstract concepts into tangible realities.")
+                .padding(.bottom)
+           
+            Text("By participating in these simulations, you're taking a significant step towards understanding and empathy. Let's use this knowledge to advocate for accessible environments and inclusive experiences for everyone.").padding(.bottom)
+                  
+                  Text("Together, we can make a difference—one insight at a time.").padding(.bottom)
+            // Fix alignment
+            NavigationLink("Let's try") {
+                VisualDisabilitySimulationView()
+            }
+            .buttonBorderShape(.capsule)
+            .buttonStyle(.bordered)
+        }
+    }
+}
+
+#Preview {
+    SimulationRationaleView()
+}
